c1aaea2a60f5b4502df101e38fa528ab0e9f5d05
paaulorobson <p.robson085@gmail.com>
2020-11-09 22:56:05 -0300

    Conectando com api do github.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,55 +1,72 @@
-import React from 'react'
-import { FiChevronRight } from 'react-icons/fi'
+import React, { useState, FormEvent } from "react";
+import { FiChevronRight } from "react-icons/fi";
+import api from "../../services/api";
 
-import logoImg from '../../assets/logo.svg'
+import logoImg from "../../assets/logo.svg";
 
-import {Title, Form, Repositories} from './styles'
+import { Title, Form, Repositories } from "./styles";
+
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+
+    }
+}
 
 // dizendo que o react é uma function component (padrão quando se usa Typescript)
 const Dashboard: React.FC = () => {
-    return (
-        <>
-            <img src={logoImg} alt="Github explorer" />
-            <Title>Explore repositórios no Github</Title>
+  const [input, setInput] = useState('')  
+  const [repositorios, setRepositorios] = useState<Repository[]>([]);
 
-            <Form>
-                <input type="text" placeholder="Digite o nome do repositório"/>
-                <button type="submit">Pesquisar</button>
-            </Form>
+ async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+      //adição de um novo repositório..
+      //consumir api do github.
+      //salvar novo repositório no estado.
+      event.preventDefault()
+      const response = await api.get<Repository>(`repos/${input}`)
+      const repository = response.data
 
-            <Repositories>
-                <a href="">
-                    <img src="https://avatars3.githubusercontent.com/u/36008926?s=460&u=93504f64ca3f549ab847b79393b03e11a752f659&v=4" alt="Paulo Robson"/>
-                    <div>
-                        <strong>paaulorobson/pokerapi</strong>
-                        <p>Interface que integra com api do pokemon</p>
-                    </div>
+      setRepositorios([...repositorios, repository])
+      setInput('')
 
-                    <FiChevronRight size={20} color="#C9C9D4"/>
-                </a>
 
-                <a href="">
-                    <img src="https://avatars3.githubusercontent.com/u/36008926?s=460&u=93504f64ca3f549ab847b79393b03e11a752f659&v=4" alt="Paulo Robson"/>
-                    <div>
-                        <strong>paaulorobson/pokerapi</strong>
-                        <p>Interface que integra com api do pokemon</p>
-                    </div>
+  }
 
-                    <FiChevronRight size={20} color="#C9C9D4"/>
-                </a>
+  return (
+    <>
+      <img src={logoImg} alt="Github explorer" />
+      <Title>Explore repositórios no Github</Title>
 
-                <a href="">
-                    <img src="https://avatars3.githubusercontent.com/u/36008926?s=460&u=93504f64ca3f549ab847b79393b03e11a752f659&v=4" alt="Paulo Robson"/>
-                    <div>
-                        <strong>paaulorobson/pokerapi</strong>
-                        <p>Interface que integra com api do pokemon</p>
-                    </div>
+      <Form onSubmit={handleAddRepository}>
+        <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)} 
+            type="text" 
+            placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-                    <FiChevronRight size={20} color="#C9C9D4"/>
-                </a>
-            </Repositories>
-        </>
-        )
-}
+      <Repositories>
+        {repositorios.map(repository => (
+            <a key={repository.full_name} href="teste">
+            <img
+                src={repository.owner.avatar_url}
+                alt={repository.owner.login}
+            />
+            <div>
+                <strong>{repository.full_name}</strong>
+                <p>{repository.description}</p>
+            </div>
+  
+            <FiChevronRight size={20} color="#C9C9D4" />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
